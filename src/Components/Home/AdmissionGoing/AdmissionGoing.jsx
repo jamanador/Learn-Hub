7ba987e38cf401d411a6e53004fe6ce956a0c719
Ador Zamaan <adorzamaan@gmail.com>
2023-05-19 22:15:01 +0600

    admissionGoing part added

--- src/Components/Home/AdmissionGoing/AdmissionGoing.jsx
+++ src/Components/Home/AdmissionGoing/AdmissionGoing.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './AdmissionGoing.css';
 const AdmissionGoing = () => {
-    const admissions = ["Unlock your future with us.", "Quality education for a prosperous future." ,"Taking your career to new heights."]
+ 
     return (
-        <div className='text-center py-8'>
-            <h3 className='font-bold py-4 lg:text-2xl text-sm'>Admission Going On</h3>
-            {
-                admissions?.map((admission,index)=> <li key={index} admission={admission}>{admission}</li>)
-            }
-            <Link to='/courses'><button className="lg:px-16 px-8 py-2 bg-purple-700 text-white">Apply Now</button></Link>
+        <div className='my-16 text-center py-12 text-white dark:text-white bg'>
+           <div className='flex items-center justify-center'>
+           <h3 className='font-bold py-4 lg:text-2xl text-sm'>Admission Going </h3>
+           <div className='w-7 h-7 border-4 border-dashed rounded-full animate-spin ml-2 mr-1 border-green-600'></div>n
+           </div>
+            <ul className='flex justify-center list-none'>
+                <li className='px-2 text-lg'> ✔ Unlock your future with us.</li>
+                <li className='px-2 text-lg'> ✔ Quality education for a prosperous future.</li>
+                <li className='px-2 text-lg'> ✔ Taking your career to new heights.</li>
+            </ul>
+            <Link to='/courses'><button className="lg:px-16 px-8 py-2 lg:my-4 my-2 bg-purple-700 text-white">Apply Now</button></Link>
         </div>
     );
 };
